5e30517af24573b02620f964e1c7b4423ab75a61
huynhlamdangkhoa <109946217+huynhlamdangkhoa@users.noreply.github.com>
2023-04-26 10:29:58 +0700

    Update GameThread.java

--- src/GUI/GameThread.java
+++ src/GUI/GameThread.java
@@ -1,18 +1,23 @@
 package GUI;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class GameThread extends Thread{
     private GameArea gameArea;
 
     public GameThread (GameArea gameArea){
         this.gameArea = gameArea;
     }
+    @Override
     public void run() {
         while (true) {
-            try {
-                gameArea.moveBlockDown();
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            gameArea.spawnBlock();
+            while (gameArea.moveBlockDown()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, e);
+                }
             }
         }
     }
