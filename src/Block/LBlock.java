37146a10e7cb33b30de4342e8cd85a6442c360b8
Huỳnh Lâm Đăng Khoa <109946217+huynhlamdangkhoa@users.noreply.github.com>
2023-05-28 00:13:06 +0700

    Update LBlock.java

--- src/Block/LBlock.java
+++ src/Block/LBlock.java
@@ -1,3 +1,11 @@
+/*Name: Group 17
+  Nguyễn Khánh Hà - ITCSIU21004
+  Huỳnh Lâm Đăng Khoa - ITCSIU21138
+  Nguyễn Bình Phương Huy - ITCSIU21189
+  Trần Thanh Nguyên - ITCSIU21093
+  Purpose: This is a class to create L block*/
+
+
 package Block;
 
 public class LBlock extends TetrisBlock{
